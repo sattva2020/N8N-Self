6090c74589b3e72d63df68e72ecbb0131743033a
sattva2020 <ruslan.griban@gmail.com>
2025-09-10 17:02:02 +0300

    chore(ci): fix eslint config and deps to be installable in CI

--- dashboard/frontend/.eslintrc.cjs
+++ dashboard/frontend/.eslintrc.cjs
@@ -13,50 +13,17 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'prettier'
+    'prettier',
   ],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
   rules: {
     'react/prop-types': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     'no-console': 'warn',
-    'react/react-in-jsx-scope': 'off'
-  }
-}
-module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
+    'react/react-in-jsx-scope': 'off',
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  parserOptions: {
-    ecmaFeatures: { jsx: true },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  settings: { react: { version: 'detect' } },
-  rules: {
-    'no-console': 'off',
-  },
-}
-module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'react'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  rules: {
-    // project specific rules
-    'react/react-in-jsx-scope': 'off'
-  }
 };
